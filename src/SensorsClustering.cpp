cf39413d1f1f442afb404ae026ab4c114895df54
AngeloTrotta85 <angelo@magilia.com>
2017-07-20 10:42:26 +0200

    ok

--- src/SensorsClustering.cpp
+++ src/SensorsClustering.cpp
@@ -6,7 +6,19 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>     // std::cout
+#include <fstream>      // std::ifstream
+#include <algorithm>    // std::find
+#include <vector>       // std::vector
+#include <list>       // std::list
+#include <cstdlib>
+#include <ctime>
+
+#include <boost/range/irange.hpp>
+#include "MyCoord.h"
+
 using namespace std;
 
 int main() {
