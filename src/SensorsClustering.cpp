b328701b16e9bb446f1067ed807dc337a8292e78
AngeloTrotta85 <angelo@magilia.com>
2017-07-20 10:37:28 +0200

    Initial commit

---
+++ src/SensorsClustering.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SensorsClustering.cpp
+// Author      : Angelo Trotta
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
